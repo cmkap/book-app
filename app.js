d4aed9ad380b1d3ffaaa5854f6e087e13d2fbbbb
Chisambwe Kapihya <c.m.kapihya@gmail.com>
2022-08-17 22:08:36 +0100

    app.js added and express framework connected

---
+++ app.js
@@ -0,0 +1,11 @@
+//entry point
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => res.send('Hello world!'));
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`server is running on port ${port}`))
