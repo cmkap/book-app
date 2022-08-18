afaae6f8135f9de3beacf227cc08a0aa859e5bdf
Chisambwe Kapihya <c.m.kapihya@gmail.com>
2022-08-18 14:06:56 +0100

    DB added and connected to app

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-//entry point
-
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
+
+// Connect Database
+connectDB()
 
 app.get('/', (req,res) => res.send('Hello world!'));
 
