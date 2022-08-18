28bcefe62d219a2d8dbf49d96adf98cf1f6f6cc9
Chisambwe Kapihya <c.m.kapihya@gmail.com>
2022-08-18 22:29:44 +0100

    MongoDB Book Schema added, Book Model added

---
+++ models/Book.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose'); //imports Mongoose API which i s an object modelling tool for MongoDB
+
+
+// Defining Book schema- singular
+// create a new instance for entry into the database
+// Mongoose starts with a shema
+// Each schema maps to a MongoDB collection and defines
+// the shape of the documents within the collection
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    isbn: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    publish_date: {
+        type: Date
+    },
+    publisher: {
+        type: String
+    },
+    update_date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+// To use our schema definition, we need to convert our bookSchema into a Model
+// Models are fancy constructors
+// an instance of a model is a document
+// Models create and read docs from DBs
+
+module.exports = Book = mongoose.model('Book', bookSchema)
+
